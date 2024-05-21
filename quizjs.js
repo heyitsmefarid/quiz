3dbf89d2e0af12e57a57f1badeb8454d796bb253
heyitsmefarid <gallanofredanthony06@gmail.com>
2024-05-21 16:04:49 +0800

    Add files via upload

---
+++ quizjs.js
@@ -0,0 +1,110 @@
+function input1(){
+    document.getElementById('label1').style.color="red";
+    document.getElementById('label3').style.color="green"
+}
+function input2(){
+    document.getElementById('label2').style.color="red";
+    document.getElementById('label3').style.color="green"
+}
+function input3(){
+    document.getElementById('label3').style.color="green";
+}
+function input4(){
+    document.getElementById('label4').style.color="green";
+}
+function input5(){
+    document.getElementById('label5').style.color="red";
+    document.getElementById('label4').style.color="green"
+}
+function input6(){
+    document.getElementById('label6').style.color="red";
+    document.getElementById('label4').style.color="green"
+}
+function input8(){
+    document.getElementById('label8').style.color="green";
+}
+function input7(){
+    document.getElementById('label7').style.color="red";
+    document.getElementById('label8').style.color="green"
+}
+function input9(){
+    document.getElementById('label9').style.color="red";
+    document.getElementById('label8').style.color="green"
+}
+function input10(){
+    document.getElementById('label10').style.color="red";
+    document.getElementById('label2').style.color="green"
+}
+function input11(){
+    document.getElementById('label11').style.color="red";
+    document.getElementById('label12').style.color="green"
+}
+function input12(){
+    document.getElementById('label12').style.color="green";
+}
+function input13(){
+    document.getElementById('label13').style.color="green";
+}
+function input14(){
+    document.getElementById('label14').style.color="red";
+    document.getElementById('label13').style.color="green"
+}
+function input15(){
+    document.getElementById('label15').style.color="red";
+    document.getElementById('label13').style.color="green"
+}
+function input16(){
+    document.getElementById('label16').style.color="green";
+}
+function input17(){
+    document.getElementById('label17').style.color="red";
+    document.getElementById('label16').style.color="green"
+}
+function input18(){
+    document.getElementById('label18').style.color="red";
+    document.getElementById('label16').style.color="green"
+}
+function input19(){
+    document.getElementById('label19').style.color="red";
+    document.getElementById('label21').style.color="green"
+}
+function input20(){
+    document.getElementById('label20').style.color="red";
+    document.getElementById('label21').style.color="green"
+}
+function input21(){
+    document.getElementById('label21').style.color="green";
+}
+function input22(){
+    document.getElementById('label22').style.color="red";
+    document.getElementById('label23').style.color="green"
+}
+function input23(){
+    document.getElementById('label23').style.color="green";
+}
+function input24(){
+    document.getElementById('label24').style.color="red";
+    document.getElementById('label23').style.color="green"
+}
+function input25(){
+    document.getElementById('label25').style.color="green";
+}
+function input26(){
+    document.getElementById('label26').style.color="red";
+    document.getElementById('label15').style.color="green"
+}
+function input27(){
+    document.getElementById('label27').style.color="red";
+    document.getElementById('label25').style.color="green"
+}
+function input28(){
+    document.getElementById('label28').style.color="red";
+    document.getElementById('label30').style.color="green"
+}
+function input29(){
+    document.getElementById('label29').style.color="red";
+    document.getElementById('label30').style.color="green"
+}
+function input30(){
+    document.getElementById('label30').style.color="green";
+}
